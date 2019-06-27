b0e8d6dcab1f590c9d7b784035e76fcc47a5e3e3
Richard Allaway <richard.allaway@gmail.com>
2019-06-27 12:56:48 -0400

    add mapToLatestFrom operator helper

--- src/app/modules/rxjs-helpers.ts
+++ src/app/modules/rxjs-helpers.ts
@@ -1,5 +1,14 @@
-import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { tap, withLatestFrom } from 'rxjs/operators';
 
 export function debug<T>(message) {
     return tap<T>(val => console.info(message, val), console.error, () => console.log(message, 'COMPLETED'));
+}
+
+export function mapToLatestFrom<T>(observable$:Observable<T>) {
+    return <U>(sourceObservable$: Observable<U>): Observable<T> => {
+        return sourceObservable$.pipe(
+            withLatestFrom(observable$, ((_, term) => term)),
+        );
+    }
 }
