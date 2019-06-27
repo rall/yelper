38f823b96294bcd114f619b3167b743b1e5a99f2
Richard Allaway <richard.allaway@gmail.com>
2019-06-27 12:29:14 -0400

    implement yelp api service with native http

--- src/app/api/yelp.service.ts
+++ src/app/api/yelp.service.ts
@@ -1,25 +1,33 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { HttpParamsOptions, HttpParams } from '@angular/common/http/src/params';
+import { HTTP } from '@ionic-native/http/ngx';
+import { from, Observable } from 'rxjs';
+import { pluck, map } from 'rxjs/operators';
+import { SearchData } from '../interfaces/search-data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class YelpService {
-  baseUrl:string = "https://api.yelp.com/v3/";
+  static hostName:string = "api.yelp.com";
+  static baseUrl:string = `https://${YelpService.hostName}/v3/`;
+  static apiKey:string = "";
 
   constructor(
-    private http: HttpClient
-  ) { }
+    private http: HTTP
+  ) {
+    this.http.setHeader('api.yelp.com', 'Authorization', `Bearer ${YelpService.apiKey}`);
+  }
 
-
-  getBusinesses([term, latlng, radius]:[string, { latitude:string, longitude:string }, number]) {
-    const params = new HttpParams().
-      set('name', term).
-      set('latitude', latlng.latitude).
-      set('longitude', latlng.longitude).
-      set('radius', String(radius));
- 
-    return this.http.get("businesses/search", { params: params });
+  getBusinesses([term, coordinates, radius]:[string, Coordinates, number]):Observable<SearchData> {
+    const params = {
+      name: term,
+      radius: String(radius),
+      ...coordinates
+    };
+    return from(this.http.get(`${YelpService.baseUrl}businesses/search`, params, undefined)).pipe(
+      pluck('data'),
+      map<string, SearchData>(response => JSON.parse(response)),
+      // catchError(),
+    );
   }
 }
