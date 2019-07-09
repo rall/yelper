3cac14af7a53d0563c8edd012b97fd62f23e4514
Richard Allaway <richard.allaway@gmail.com>
2019-07-09 13:30:11 -0400

    streams for 'redo search here' logic

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Subject, combineLatest } from 'rxjs';
-import { distinctUntilChanged, switchMap, map, sample } from 'rxjs/operators';
+import { Subject, combineLatest, merge, Observable } from 'rxjs';
+import { distinctUntilChanged, switchMap, map, sample, pluck, filter, mapTo, withLatestFrom } from 'rxjs/operators';
 import { YelpService } from '../api/yelp.service';
-import { ILatLng } from '@ionic-native/google-maps/ngx';
+import { ILatLng, Spherical } from '@ionic-native/google-maps/ngx';
 import { SearchData } from '../interfaces/search-data';
 import { debug } from '../modules/rxjs-helpers';
 import { latLngToCoordinates, coordinatesToLatLng, coordinatesEquality } from '../modules/geo-helpers';
@@ -16,6 +16,7 @@ export class SearchService {
   termSubject: Subject<string> = new Subject();
   radiusSubject: Subject<number> = new Subject();
   searchSubject: Subject<SearchData> = new Subject();
+  redoReadySubject: Subject<boolean> = new Subject();
 
   constructor(
     private yelp:YelpService,
@@ -41,5 +42,41 @@ export class SearchService {
       switchMap(getBusinesses),
       debug('search'),
     ).subscribe(this.searchSubject);
+
+    const currentLatLng$ = this.searchSubject.pipe(
+      pluck("region", "center"),
+      map(coordinatesToLatLng),
+    );
+
+    const latLngDelta$ = combineLatest(currentLatLng$, this.latlngSubject).pipe(
+      map(([a,b]) => Spherical.computeDistanceBetween(a, b)),
+    );
+
+    const currentRadius$ = radius$.pipe(
+      sample(this.searchSubject),
+    );
+
+    const radiusDelta$ = combineLatest(currentRadius$, radius$).pipe(
+      map(([a, b]) => Math.abs(a - b)),
+    );
+
+    const lastRadius$:Observable<number> = radius$.pipe(
+      sample(this.triggerSubject),
+    );
+
+    merge(latLngDelta$, radiusDelta$).pipe(
+      withLatestFrom(lastRadius$),
+      filter(([delta, lastRadius]) => delta > (lastRadius / 2)),
+      mapTo(true),
+    ).subscribe(this.redoReadySubject);
+
+    combineLatest(latLngDelta$, radiusDelta$).pipe(
+      filter(([a, b]) => a== 0 && b == 0),
+      mapTo(false),
+    ).subscribe(this.redoReadySubject);
+
+    this.searchSubject.pipe(
+      mapTo(false),
+    ).subscribe(this.redoReadySubject);
   }
 }
